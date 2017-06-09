ea2100aa15ba700f3c651974411dde9051b19550
Chen-yu Li <qwe153246@gmail.com>
2017-06-09 10:11:11 +0800

    Update stm32f4xx_it.h

--- Inc/stm32f4xx_it.h
+++ Inc/stm32f4xx_it.h
@@ -46,7 +46,6 @@
 /* Exported functions ------------------------------------------------------- */
 
 void SVC_Handler(void);
-void PendSV_Handler(void);
 void SysTick_Handler(void);
 
 #ifdef __cplusplus
